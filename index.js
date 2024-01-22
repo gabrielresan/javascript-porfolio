1d6932fe25754b3e5cafe506aaf2c7cbd54bccdd
Gabriel Reis <66891140+gabrielresan@users.noreply.github.com>
2024-01-21 21:17:10 -0300

    Fix final message

--- index.js
+++ index.js
@@ -3,9 +3,9 @@ console.log("Loading");
 let userName = prompt("Digite seu nome: ");
 let idade = parseInt(prompt("Digite sua idade atual: "));
 let idadeAposentar = parseInt(
-  prompt("Digita a idade com a qual deseja se aposentar:")
+  prompt("Digite a idade com a qual deseja se aposentar:")
 );
 
 let tempoAposentar = idadeAposentar - idade;
 
-alert(`${userName}, faltam ${idadeAposentar} anos para você se aposentar!`);
+alert(`${userName}, faltam ${tempoAposentar} anos para você se aposentar!`);
